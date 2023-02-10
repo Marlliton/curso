b416c8ca4729c3739e04e2ba9a7239582c63dcfb
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2023-02-09 23:00:01 -0300

    rotas da apliação

--- fastify/src/server.ts
+++ fastify/src/server.ts
@@ -1,15 +1,15 @@
 import fastify from 'fastify'
-import { knex } from './database'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/hallo', async (request, reply) => {
-  const test = await knex('sqlite_schema').select('*')
-  return test
-})
+app.register(cookie)
+app.register(transactionsRoutes, { prefix: 'transactions' })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('Server Running!!!'))
