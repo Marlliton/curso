c6c6a4eb0c7be8b9697fc66ed7f79a7fdf3d998a
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2023-02-15 19:14:35 -0300

    primeiro commit

---
+++ backend/fastify/src/database.ts
@@ -0,0 +1,15 @@
+import { Knex, knex as knexSetup } from 'knex'
+import { env } from './env'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './database/migrations',
+  },
+}
+export const knex = knexSetup(config)
---
+++ backend/fastify/src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠ Error: Environment variable undefined', _env.error.format())
+
+  throw Error('Invalid Environment variables')
+}
+
+export const env = _env.data
