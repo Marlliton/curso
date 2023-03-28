78c7b721b64bbfb0d5e1e724dfa06ff8bd549404
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2023-03-28 10:36:52 -0300

    Configuração inicial

---
+++ shop/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$green300",
+  borderRadius: 6,
+  fontSize: "1.5rem",
+  padding: ".5rem 1rem",
+});
+
+export default function Home() {
+  return <div>Teste</div>;
+}
