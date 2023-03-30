3e0f36600511b9889c0e5ac1804673b5e1057bfb
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2023-03-29 23:20:31 -0300

    Criação do carousel

--- shop/src/pages/index.tsx
+++ shop/src/pages/index.tsx
@@ -1,12 +1,57 @@
-import { styled } from "../styles";
-
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 6,
-  fontSize: "1.5rem",
-  padding: ".5rem 1rem",
-});
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import camisa01 from "../assets/shirt.png";
+import camisa02 from "../assets/shirt-1.png";
+import camisa03 from "../assets/shirt-2.png";
+import { Carousel } from "../components/Carousel";
 
 export default function Home() {
-  return <div>Teste</div>;
+  return (
+    <HomeContainer>
+      <Carousel perPage={2}>
+        <Product href="#">
+          <Image src={camisa01} alt="Camisa 01" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79.99</span>
+          </footer>
+        </Product>
+        <Product href="#">
+          <Image src={camisa02} alt="Camisa 01" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79.99</span>
+          </footer>
+        </Product>
+        <Product href="#">
+          <Image src={camisa01} alt="Camisa 01" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79.99</span>
+          </footer>
+        </Product>
+        <Product href="#">
+          <Image src={camisa03} alt="Camisa 01" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79.99</span>
+          </footer>
+        </Product>
+        <Product href="#">
+          <Image src={camisa03} alt="Camisa 01" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79.99</span>
+          </footer>
+        </Product>
+        <Product href="#">
+          <Image src={camisa03} alt="Camisa 01" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79.99</span>
+          </footer>
+        </Product>
+      </Carousel>
+    </HomeContainer>
+  );
 }
---
+++ shop/src/styles/pages/home.ts
@@ -0,0 +1,66 @@
+import { styled } from "..";
+
+export const HomeContainer = styled("main", {
+  display: "flex",
+  width: "100%",
+  maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
+  marginLeft: "auto",
+  gap: "3rem",
+
+  minHeight: 656,
+});
+
+export const Product = styled("a", {
+  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
+  borderRadius: "8px",
+
+  position: "relative",
+
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: ".25rem",
+
+  minWidth: "30rem",
+
+  overflow: "hidden",
+
+  img: {
+    objectFit: "cover",
+  },
+
+  footer: {
+    position: "absolute",
+    margin: 4,
+    bottom: ".25rem",
+    left: ".25rem",
+    right: ".25rem",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    borderRadius: "6px",
+
+    padding: "2rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    transform: "translateY(110%)",
+    opacity: 0,
+    transition: "all .2s ease-in-out",
+
+    fontSize: "$lg",
+    color: "$gray100",
+
+    span: {
+      color: "$green300",
+      fontSize: "$xl",
+      fontWeight: "bold",
+    },
+  },
+
+  "&:hover": {
+    footer: {
+      transform: "translateY(0%)",
+      opacity: 1,
+    },
+  },
+});
