39917873f3422268b8f029ee7dde49e4c6a91d34
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2023-04-11 19:31:05 -0300

    Documentaçde tokens

--- React/design-system/packages/react/src/index.tsx
+++ React/design-system/packages/react/src/index.tsx
@@ -1,5 +1,10 @@
-import { colors } from '@ui/tokens'
+import { styled } from "../styles";
 
-export function App() {
-  return <h1 style={{ color: colors.ignite700 }}>Hallo World.</h1>
-}
+export const Button = styled("button", {
+  backgroundColor: "$ignite500",
+  color: "$gray100",
+  fontWeight: "$medium",
+  borderRadius: "$md",
+  border: 0,
+  padding: "$4",
+});
