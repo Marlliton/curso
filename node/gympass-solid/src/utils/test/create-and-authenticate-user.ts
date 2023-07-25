cfa8bbe8df3329ea32328fd3d6ab1b547beacafa
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2023-07-25 20:14:40 -0300

    github actions

--- node/gympass-solid/src/utils/test/create-and-authenticate-user.ts
+++ node/gympass-solid/src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,20 @@
+import { hash } from "bcryptjs";
 import { FastifyInstance } from "fastify";
 import request from "supertest";
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post("/users").send({
-    name: "John Doe",
-    email: "johndoe@example.com",
-    password: "123456",
+import { prisma } from "@/lib/prisma";
+
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false
+) {
+  await prisma.user.create({
+    data: {
+      email: "johndoe@example.com",
+      name: "John Doe",
+      password_hash: await hash("123456", 8),
+      role: isAdmin ? "ADMIN" : "MEMBER",
+    },
   });
 
   const {
